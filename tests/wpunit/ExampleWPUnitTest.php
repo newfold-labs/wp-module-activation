8a355ea96bab4597c3dead6f0ee8ffa6a2e75558
Carlos Rodriguez Brito <crodriguezbrito@gmail.com>
2025-04-16 13:55:13 +0100

    lint phpcs

--- tests/wpunit/ExampleWPUnitTest.php
+++ tests/wpunit/ExampleWPUnitTest.php
@@ -3,6 +3,8 @@
 namespace NewfoldLabs\WP\Module\Activation;
 
 /**
+ *  Example Unit test.
+ * 
  * @coversDefaultClass \NewfoldLabs\WP\Module\Activation\Example
  */
 class ExampleWPUnitTest extends \lucatume\WPBrowser\TestCase\WPTestCase {
