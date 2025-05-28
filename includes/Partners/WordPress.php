f03c3bfcf816b4822b7cea65787386b43c42b14e
amartya-dev <amarkaushik1999@gmail.com>
2025-05-28 21:10:00 +0530

    lint fixes

--- includes/Partners/WordPress.php
+++ includes/Partners/WordPress.php
@@ -12,27 +12,27 @@ namespace NewfoldLabs\WP\Module\Activation\Partners;
  */
 class WordPress extends Partner {
 
-    /**
-     * Initialize.
-     *
-     * @return void
-     */
-    public function init() {
-        add_action('admin_init', array($this, 'disable_welcome_panel'));
-    }
+	/**
+	 * Initialize.
+	 *
+	 * @return void
+	 */
+	public function init() {
+		add_action('admin_init', array($this, 'disable_welcome_panel'));
+	}
 
-    /**
-     * Disable WordPress welcome panel.
-     *
-     * @return void
-     */
-    public function disable_welcome_panel() {
-        // Remove the welcome panel
-        remove_action('welcome_panel', 'wp_welcome_panel');
-        
-        // Set user meta to hide welcome panel
-        if (get_current_user_id() > 0) {
-            update_user_meta(get_current_user_id(), 'show_welcome_panel', 0);
-        }
-    }
+	/**
+	 * Disable WordPress welcome panel.
+	 *
+	 * @return void
+	 */
+	public function disable_welcome_panel() {
+		// Remove the welcome panel
+		remove_action('welcome_panel', 'wp_welcome_panel');
+		
+		// Set user meta to hide welcome panel
+		if (get_current_user_id() > 0) {
+			update_user_meta(get_current_user_id(), 'show_welcome_panel', 0);
+		}
+	}
 }
